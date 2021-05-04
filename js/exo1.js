4f7f0c8464ee84e903bdf15bb30b76df926cd50f
mathilderenversade <mathirenversade@gmail.com>
2021-05-04 12:30:47 +0200

    exo 1 version module

--- js/exo1.js
+++ js/exo1.js
@@ -1,4 +1,4 @@
-// Je vais initialiser une variable avec mon nombre de clic
+/*// Je vais initialiser une variable avec mon nombre de clic
 var clickNumber = 0;
 
 // Je cible mon élément du DOM sur lequel j'ai envie de mettre un écouteur.
@@ -19,5 +19,32 @@ var handleClick = function(){
 }
 // Je crée un écouteur d'événement sur mon bouton
 monBouton.addEventListener('click', handleClick);
+*/
 
+// Les Modules en Javascript
+// N'importe quel programme qu'on crée, s'organise selon des thématiques ou grandes fonctionnalités comme par exemple : les comptes utilisateurs, Afficher des scores, Emprunter un livre. L'idée c'est de créer des modules pour exploiter dans plusieurs projets les mêmes briques de code. Ca permet aussi de compartimenter mon code, chaque thématique étant isolée dans un module. 
+
+// Par convention, le module général s'appelle app et on y inclue une méthode init() qui va servir de point d'entrée dans le module. Init contient tous les traitements qui lancent l'application. 
+
+var app = {
+    // j'initialise une propriété de mon objet qui va contenir le nombre de clic
+    clickNumber: 0,
+
+    handleClick: function(){
+        // j'incrémente la propriété clickNumber de mon objet app.
+        app.clickNumber++;
+        var compteur = document.getElementById('counter');
+        console.log(compteur);
+        compteur.innerHTML = app.clickNumber;
+    },
+
+    init: function(){
+        // cible mon élément bouton
+        var monBouton = document.querySelector('button');
+        // je mets mon bouton sur écoute au click
+        monBouton.addEventListener('click', app.handleClick);
+    }
+};
+console.log(app);
+app.init();
 
